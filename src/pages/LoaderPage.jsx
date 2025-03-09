908bbc95aaedffca82c612cd96f1407f9db401ba
shihabhq <adnansaim15@yahoo.com>
2025-03-10 03:19:52 +0600

    loader added

--- src/pages/LoaderPage.jsx
+++ src/pages/LoaderPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
-import { XlviLoader } from "react-awesome-loaders";
 import { useLocation } from "react-router";
+import Loading from "../shared/Loading";
 
 const LoaderPage = ({ children }) => {
   const [loader, setLoader] = useState(true);
@@ -18,11 +18,7 @@ const LoaderPage = ({ children }) => {
   if (loader) {
     return (
       <section className="h-screen w-full flex items-center justify-center">
-        <XlviLoader
-          boxColors={["#fff", "#66fcf1", "#00c950"]}
-          desktopSize={"158px"}
-          mobileSize={"100px"}
-        />
+        <Loading />
       </section>
     );
   }
---
+++ src/shared/Loading.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./loader.css"; // We'll create this next
+
+const Loading = () => {
+  return (
+    <div className="loader">
+      <div className="box1"></div>
+      <div className="box2"></div>
+      <div className="box3"></div>
+    </div>
+  );
+};
+
+export default Loading;
